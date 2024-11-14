403d2ef9cd039d1b9d8ca52db4f465ab1aac067d
rodantinarelli <rodantinarelli@digitalocean.com>
2024-11-14 12:12:04 -0800

    fix recursion

--- index.js
+++ index.js
@@ -4,25 +4,35 @@ const { Client } = require('@notionhq/client');
 const notion = new Client({ auth: process.env.NOTION_API_KEY });
 const PARENT_PAGE_ID = process.env.PARENT_PAGE_ID;
 
-const removeCheckedItems = async () => {
-  const fetchPageContent = async (blockId) => {
-    const blocks = [];
-    let cursor;
-    do {
-      const { results, next_cursor } = await notion.blocks.children.list({
-        block_id: blockId,
-        start_cursor: cursor,
-      });
-      blocks.push(...results);
-      cursor = next_cursor;
-    } while (cursor);
-    return blocks;
-  };
+const fetchPageContent = async (blockId) => {
+  const blocks = [];
+  let cursor;
+  do {
+    const { results, next_cursor } = await notion.blocks.children.list({
+      block_id: blockId,
+      start_cursor: cursor,
+    });
+    blocks.push(...results);
+    cursor = next_cursor;
+  } while (cursor);
 
+  // Recursively fetch children blocks
+  for (const block of blocks) {
+    if (block.has_children) {
+      block.children = await fetchPageContent(block.id);
+    }
+  }
+
+  return blocks;
+};
+
+const removeCheckedItems = async () => {
   const deleteDoneItems = async (blocks) => {
     for (const block of blocks) {
       if (block.type === 'to_do' && block.to_do.checked) {
         await notion.blocks.delete({ block_id: block.id });
+      } else if (block.children) {
+        await deleteDoneItems(block.children);
       }
     }
   };
@@ -56,6 +66,7 @@ const removeCheckedItems = async () => {
                 content: `Last Updated: ${formattedDate}`,
               },
               annotations: {
+                color: 'gray',
                 italic: true,
               },
             },
